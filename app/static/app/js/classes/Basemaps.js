ec19eab7ad039beeddcd478406f3a8bf06103e2e
weihuizhong <383041894@qq.com>
2025-08-21 20:07:52 +0800

    feature:修改地图源显示

--- app/static/app/js/classes/Basemaps.js
+++ app/static/app/js/classes/Basemaps.js
@@ -2,28 +2,31 @@ import { _ } from './gettext';
 
 export default [
   {
-    attribution: "Map data: &copy; Google Maps",
-    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+    attribution: "谷歌地图",
+    subdomains: [
+      "7abac8c11716f6949e7b23f76e60fcd0",
+      "444c94a4157de3c06c435132eb2f1ac5",
+    ],
     maxZoom: 21,
-    minZoom: 0,
-    label: _("Google Maps Hybrid"),
-    url: "//{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+    minZoom: 3,
+    label: _("谷歌地图"),
+    url: "https://maps.hk.sumfen.com/{s}/kh?v=904&hl=zh-CN&x={x}&y={y}&z={z}",
   },
   {
-    attribution:
-      "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-    maxZoom: 21,
-    minZoom: 0,
-    label: _("ESRI Satellite"),
-    url:
-      "//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+    attribution: "星图地球",
+    maxZoom: 18,
+    minZoom: 3,
+    label: _("星图地球"),
+    url: "http://tiles1.geovisearth.com/base/v1/img/{z}/{x}/{y}?format=webp&tmsIds=w&token=afba1316bcd8e1e58d95ee480a61369fd542660819848ac9ef191c563ded201f",
   },
   {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    maxZoom: 19,
-    minZoom: 0,
-    label: _("OpenStreetMap"),
-    url: "//tile.openstreetmap.org/{z}/{x}/{y}.png"
-  }
+    attribution: "天地图",
+    minZoom: 3,
+    maxZoom: 18,
+    subdomains: ['0', '1', '2', '3', '4', '5', '6', '7'],
+    key: "bd41a94262676f6696917728b37a5663",
+    detectRetina: true,
+    label: _("天地图"),
+    url: "https://t{s}.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk={key}",
+  },
 ];
